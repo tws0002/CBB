8ec025df42dc2f3f0f29798c79621c1e0031eae2
worrytron <rastermancer@gmail.com>
2017-06-02 18:27:14 -0400

    -

--- lib/aeCore.jsx
+++ lib/aeCore.jsx
@@ -261,4 +261,12 @@ String.prototype.format = function() {
     return formatted;
 };
 
+function zeroFill( number, width ){
+  width -= number.toString().length;
+  if ( width > 0 ) {
+    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+  }
+  return number + ""; // always return a string
+}
+
 /** EOF **/
